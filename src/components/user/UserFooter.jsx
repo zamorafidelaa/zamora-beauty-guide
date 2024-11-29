8d6a87b94637fecfc39fe4f868e1aa08e0a54e3c
zamorafidelaa <morafidela1@gmail.com>
2024-11-29 19:10:21 +0700

    ninth

--- src/components/user/UserFooter.jsx
+++ src/components/user/UserFooter.jsx
@@ -13,6 +13,7 @@ function UserFooter() {
         <div className="footer-links">
           <h4>Quick Links</h4>
           <ul>
+            <li><a href="/home">Home</a></li>
             <li><a href="/about">About Us</a></li>
             <li><a href="/consultation">Consultation</a></li>
             <li><a href="/product">Product</a></li>
@@ -22,9 +23,9 @@ function UserFooter() {
         <div className="footer-right">
           <h4>Follow Us</h4>
           <div className="social-icons">
-          <Facebook /><a href="https://www.facebook.com/morafidela?mibextid=ZbWKwL" target="_blank">Facebook</a>
-          <Instagram /><a href="https://www.instagram.com/fi.moriii/" target="_blank">Instagram</a>
-          <MessageCircleHeart /><a href="https://wa.me/087763323044" target="_blank">Whatsapp</a>
+          <a href="https://www.facebook.com/morafidela?mibextid=ZbWKwL" target="_blank"><Facebook /></a>
+          <a href="https://www.instagram.com/fi.moriii/" target="_blank"><Instagram /></a>
+          <a href="https://wa.me/087763323044" target="_blank"><MessageCircleHeart /></a>
           </div>
         </div>
       </div>
