fdd0c4a660606d66e6fc0306ad5b44ed45d0500b
zamorafidelaa <morafidela1@gmail.com>
2025-09-02 01:33:16 +0700

    new update json

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,18 +1,18 @@
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children, role }) => {
-  const userRole = localStorage.getItem("role"); 
-  const isLoggedIn = !!userRole; 
+// const ProtectedRoute = ({ children, role }) => {
+//   const userRole = localStorage.getItem("role"); 
+//   const isLoggedIn = !!userRole; 
 
-  if (!isLoggedIn) {
-    return <Navigate to="/login" />;
-  }
+//   if (!isLoggedIn) {
+//     return <Navigate to="/login" />;
+//   }
 
-  if (role && userRole !== role) {
-    return <Navigate to="/home" />;
-  }
+//   if (role && userRole !== role) {
+//     return <Navigate to="/home" />;
+//   }
 
-  return children; 
-};
+//   return children; 
+// };
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
